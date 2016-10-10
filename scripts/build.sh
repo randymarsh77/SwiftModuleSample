741e8f4949332b27058ea330860a78319217783d
Matt Preucil <mattpsbs@gmail.com>
2016-10-09 17:04:16 -0700

    Update build.sh
    Also update to Swift 3.0

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,22 +1,64 @@
-ROOT=$(pwd)/..
-MODULE=Module
+#! /bin/bash
+
+ROOT=$(dirname "${BASH_SOURCE}")/..
+MODULE=astream
 LIB=lib$MODULE.a
-TARGET=$ROOT/app/lib
+TARGET=$SOURCE_ROOT/lib
+TARGET_SDK=$SDKROOT
 
-pushd $ROOT
-cp -r lib/src temp-build
-pushd temp-build
+if test "${SDKROOT#*Simulator}" != "$SDKROOT"
+then
+TARGET_SDK=$(xcrun --show-sdk-path -sdk iphoneos)
+fi
 
-xcrun swiftc -emit-library -emit-object *.swift -sdk $(xcrun --show-sdk-path --sdk macosx) -module-name $MODULE
+function fail_build_on_error
+{
+	if [ $? != 0 ]
+	then
+		echo "Failing build"
+		popd > /dev/null
+		rm -rf temp-build
+		popd > /dev/null
+		exit 1
+	fi
+}
+
+pushd $ROOT > /dev/null
+echo "Building ${LIB} from $(pwd)"
+echo "Using SDK: ${TARGET_SDK}"
+echo "Exporting artifacts to ${TARGET}"
+
+mkdir temp-build
+cp -r src temp-build
+
+pushd temp-build/src > /dev/null
+
+LIB_SOURCE=$(ls *.swift)
+
+echo "Building object files from source..."
+echo $LIB_SOURCE
+
+xcrun swiftc -emit-library -emit-object $LIB_SOURCE -sdk $TARGET_SDK -module-name $MODULE
+fail_build_on_error
+
+echo "Archiving..."
 
 ar rcs $LIB *.o
+fail_build_on_error
 
-xcrun swiftc -emit-module *.swift -sdk $(xcrun --show-sdk-path --sdk macosx) -module-name $MODULE
+echo "Emitting module.."
+
+xcrun --toolchain swift swiftc -emit-module *.swift -sdk $TARGET_SDK -module-name $MODULE
+fail_build_on_error
+
+echo "Copying artifacts..."
 
 mv $LIB $TARGET/$LIB
 mv $MODULE.swiftdoc $TARGET/$MODULE.swiftdoc
 mv $MODULE.swiftmodule $TARGET/$MODULE.swiftmodule
 
-popd
+popd > /dev/null
 rm -rf temp-build
-popd
+popd > /dev/null
+
+echo "Finished building ${LIB}"
